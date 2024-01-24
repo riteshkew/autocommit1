d6fc773489a3eadd0dd2ff41d0073fd73e029f73
Demo Appsmith <demo@appsmith.com>
2024-01-24 13:45:53 +0000

    System generated commit, initial commit

---
+++ pages/More Widgets/jsobjects/JSObject2/JSObject2.js
@@ -0,0 +1,24 @@
+export default {
+	
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		
+		[
+  {
+    "label": "Blue",
+    "value": "BLUE"
+  },
+  {
+    "label": "Green",
+    "value": "GREEN"
+  },
+  {
+    "label": "Red",
+    "value": "RED"
+  }
+]
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+	}
+}
